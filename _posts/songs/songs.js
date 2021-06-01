89b05d95cef7039374849baa7e6fbd0f8c960245
Tatiana Barbone <tbarbone@sandiego.edu>
2021-05-31 20:39:59 -0700

    songs

---
+++ _posts/songs/songs.js
@@ -0,0 +1,8 @@
+
+        VanillaTilt.init(document.querySelectorAll(".card"), {
+            max: 25,
+            speed: 400,
+            glare: true,
+            "max-glare": 1
+        });
+       
